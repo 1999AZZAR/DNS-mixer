b8fea5ba53fe49b4e5be626315c4c32fe8ac520f
azzar budiyanto <azzar.mr.zs@gmail.com>
2024-01-27 15:59:40 +0700

    simplify more

--- code/all_ui/all_ui.py
+++ code/all_ui/all_ui.py
@@ -28,6 +28,14 @@ TOTAL_REQUESTS = 0
 TOTAL_SUCCESS = 0
 TOTAL_REJECT = 0
 
+# Function to perform blinking
+def blink(num_blinks, on_duration, off_duration):
+    for _ in range(num_blinks):
+        LED.value(1)
+        time.sleep(on_duration)
+        LED.value(0)
+        time.sleep(off_duration)
+
 # Function to connect to WiFi
 def connect_to_wifi():
     sta_if = network.WLAN(network.STA_IF)
@@ -40,41 +48,26 @@ def connect_to_wifi():
         time.sleep(1)
 
     print("Connected to WiFi")
-
-    for _ in range(5):
-        LED.value(1)
-        time.sleep(0.2)
-        LED.value(0)
-        time.sleep(0.3)
+    blink(5, 0.2, 0.3)
 
 # Function to handle DNS requests
 def handle_dns_request(data, addr):
     print("Received DNS request from:", addr)
-
-    # Blink LED for 0.05 seconds on new request
-    LED.value(1)
-    time.sleep(0.05)
-    LED.value(0)
+    blink(1, 0.05, 0)
 
     success = False
 
-    # Iterate through each DNS provider and try to resolve using the first one that responds
     for dns_provider in DNS_PROVIDERS_IPV4:
         print("Trying DNS provider:", dns_provider)
 
-        # Create a UDP socket to forward the DNS request
         dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         dns_socket.settimeout(0.03)
 
         try:
             dns_socket.sendto(data, (dns_provider, 53))
             response, _ = dns_socket.recvfrom(1024)
-            # Forward the DNS response to the original requester
             server_socket.sendto(response, addr)
-            # Blink LED for 0.02 second on success
-            LED.value(1)
-            time.sleep(0.02)
-            LED.value(0)
+            blink(1, 0.02, 0)
             success = True
             break
         except:
@@ -83,10 +76,7 @@ def handle_dns_request(data, addr):
             dns_socket.close()
 
     if not success:
-        # Blink LED for half second on failure
-        LED.value(1)
-        time.sleep(0.5)
-        LED.value(0)
+        blink(1, 0.5, 0)
         print("Failed to forward DNS request")
 
     return success
@@ -133,14 +123,12 @@ while True:
     data, addr = server_socket.recvfrom(1024)
     success = handle_dns_request(data, addr)
 
-    # Update counters
     TOTAL_REQUESTS += 1
     if success:
         TOTAL_SUCCESS += 1
     else:
         TOTAL_REJECT += 1
 
-    # Update OLED display with counters and device identifier
     oled.fill(0)
     oled.text(DEVICE_IDENTIFIER, 30, 0, 1)
     oled.text("IP:" + STATIC_IP, 0, 12, 1)
@@ -149,6 +137,4 @@ while True:
     oled.text("Reject: " + str(TOTAL_REJECT), 0, 48, 1)
     oled.show()
 
-    # Sleep for a short duration to allow the web server and OLED update to handle requests
     time.sleep(0.3)
-
